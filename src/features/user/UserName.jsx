bd478d375c5a2171d7d9b07eb94d3e5549723907
Anna <80330820+AnnaKilchytska@users.noreply.github.com>
2024-03-29 23:26:31 +0100

    add redux for state management

--- src/features/user/UserName.jsx
+++ src/features/user/UserName.jsx
@@ -1,3 +1,11 @@
+import { useSelector } from 'react-redux';
+
 export default function UserName() {
-  return <div className="hidden text-sm font-semibold md:block">Anna</div>;
+  const username = useSelector((state) => state.user.username);
+
+  if (!username) return null;
+
+  return (
+    <div className="hidden text-sm font-semibold md:block">{username}</div>
+  );
 }
